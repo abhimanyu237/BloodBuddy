d79604b6331d350704ce8e68a21dcf0ca08e614b
Anujkes <anujkes96@gmail.com>
2023-04-05 23:25:52 +0530

    Added splash screen

---
+++ app/src/main/java/com/example/bloodbuddy/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.bloodbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.example.bloodbuddy.authentication.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                    startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+
+
+                finish();
+            }
+        },2000);
+
+    }
+}
