aa40b81c93ffab9e1b3579b511cee47109e9e03d
Gautom Agarwal <garggautam72@gmail.com>
2023-04-14 15:52:24 +0530

    update request fragment

--- app/src/main/java/com/example/bloodbuddy/ui/request/RequestFragment.java
+++ app/src/main/java/com/example/bloodbuddy/ui/request/RequestFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import com.example.bloodbuddy.R;
@@ -22,6 +24,7 @@ public class RequestFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    View view;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -63,6 +66,11 @@ public class RequestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request, container, false);
+        view =  inflater.inflate(R.layout.fragment_request, container, false);
+        AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.blood_group_dropdown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.blood_group_array, android.R.layout.simple_spinner_item);
+        autoCompleteTextView.setAdapter(adapter);
+
+        return view;
     }
 }
