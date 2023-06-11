2ab6912ff2ca17316374b74a38a29d9226547dab
Anujkes <anujkes96@gmail.com>
2023-06-11 18:57:45 +0530

    getUserId() function added

--- app/src/main/java/com/example/bloodbuddy/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bloodbuddy/ui/home/HomeFragment.java
@@ -9,11 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.bloodbuddy.R;
+import com.example.bloodbuddy.UserData;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -71,11 +78,28 @@ public class HomeFragment extends Fragment {
         getActivity().setTitle(R.string.app_name);
     }
 
+    private TextView userId;
+    private String uid=null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    View view=inflater.inflate(R.layout.fragment_home, container, false);
+
+
+    userId=view.findViewById(R.id.userId);
+
+
+
+    getUserId();
+
+//    userId.setText(uid);
+
+
+
+
+
+     return view;
     }
 
     @Override
@@ -85,9 +109,9 @@ public class HomeFragment extends Fragment {
         imageList.add(new SlideModel("https://eaglestaleonline.com/wp-content/uploads/2020/10/News-Vein-Drain-Infographic-2.png","Motivation",ScaleTypes.CENTER_CROP));
         imageList.add(new SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoRTL57Z-ux_hpkpGfaMlJPj4iwvOEDqQ-dA&usqp=CAU", "Blood Donor Motivation",ScaleTypes.CENTER_CROP));
         imageList.add(new SlideModel("https://i.ytimg.com/vi/87aP7m9NcyM/maxresdefault.jpg", "“HIGHEST BLOOD DONOR\" is achieved by Hony Capt Dr. Suresh Kumar Saini from Karnal (Haryana) India on 22nd October 2020. He donated blood 131 times. He also donated platelets for 94 times.",ScaleTypes.CENTER_CROP));
-
         ImageSlider imageSlider = view.findViewById(R.id.image_slider);
         imageSlider.setImageList(imageList);
+
         ArrayList<SlideModel> imageList1 = new ArrayList<>(); // Create image list
 
         imageList1.add(new SlideModel("https://c8.alamy.com/comp/2M4KYNM/blood-donationorgan-transplantation-laboratory-mobile-app-page-onboard-screenvolunteer-character-healthcarecharity-world-donor-day-concept-for-we-2M4KYNM.jpg", "blood Buddy", ScaleTypes.CENTER_CROP));
@@ -95,5 +119,34 @@ public class HomeFragment extends Fragment {
         imageList1.add(new SlideModel("https://bit.ly/3fLJf72", "And people do that.",ScaleTypes.CENTER_CROP));
         ImageSlider imageSlider1=view.findViewById(R.id.image_slider1);
         imageSlider1.setImageList(imageList1);
+    }
+
+
+    private void getUserId(){
+
+
+        FirebaseDatabase.getInstance().getReference("users")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()).child("userId")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                    @Override
+                    public void onSuccess(DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.exists())
+                        {
+                            uid="USER ID : "+dataSnapshot.getValue(String.class);
+
+                            userId.setText(uid);
+                        }
+                        else
+                        {
+                            Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+
+
     }
 }
